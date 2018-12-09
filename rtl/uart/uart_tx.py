ea2424f0fd18479667efc85094c3beb92fd3acbe
ImArcangel <rafael.arcangel12@gmail.com>
2018-12-09 06:11:41 -0400

    tx_state_machine added

--- rtl/uart/uart_tx.py
+++ rtl/uart/uart_tx.py
@@ -7,9 +7,45 @@ from coregen.utils import createSignal
 
 @hdl.block
 def uart_tx(clk_i, rst_i, tx_tick_i, dat_i, start_i, ready_o, tx_o):
-    """
-    Escriba su código acá
-    """
+  
+ 	cnt = createSignal(0,3)
+ 	data = createSignal(0,8)
+	tx_state    = hdl.enum('IDLE','DATA')
+	state = hdl.signal(tx_state.IDLE)
+  
+ 	
+
+ 	@hdl.always_seq(clk_i.posedge, reset=rst_i)
+    
+    def tx_state_m():
+    	
+    	if state == tx_state.IDLE:
+    		
+    		ready_o.next = 1
+    		
+    		if start_i:    			
+    			data.next = dat_i
+    			ready_o.next = 0
+    			state.next = tx_state.DATA
+    		else:
+    			tx_o.next = 1
+    			
+    	elif state == tx_state.DATA:
+    		
+            if tx_tick_i:
+                tx_o.next    = data[0]
+                data.next   = hdl.concat(0, data[8:1])
+                cnt.next = cnt + 1
+                if bit_cnt == 7:
+                	if tx_tick_i:
+                		tx_o.next    = 1
+                		ready_o.next = 1
+                		state.next   = tx_state.IDLE
+
+        else:
+        	state.next = tx_state.IDLE
+               			
+
     return hdl.instances()
 
 # Local Variables:
