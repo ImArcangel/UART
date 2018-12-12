26cd4e390b52873608bc927def9ec79032a83ac6
ImArcangel <rafael.arcangel12@gmail.com>
2018-12-12 11:50:30 -0400

    Driver7Seg fixed

--- build/build_Loopback.sh
+++
@@ -1,5 +0,0 @@
-xst -ifn Loopback.xst
-ngdbuild  -uc Loopback.ucf Loopback.ngc Loopback.ngd
-map -ol high -w -pr b -timing -o Loopback_map.ncd Loopback.ngd Loopback.pcf
-par -ol high -w Loopback_map.ncd Loopback.ncd Loopback.pcf
-bitgen -w Loopback.ncd Loopback.bit
